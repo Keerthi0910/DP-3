c11dbb8ca5de8a56e90ed6645412efdbc39482a5
Keerthi0910 <chundurikeerthipriya@gmail.com>
2026-09-24 23:21:34 -0500

    Add deleteAndEarn method in Solution class
    Implement deleteAndEarn function to maximize points by deleting numbers.

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,35 @@
+// o( M) space complexity where M is max
+// o(n + M) time complexity
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int m = nums.length;
 
+     
+
+        int max = 0;
+
+        for(int i = 0; i <nums.length; i++){
+            max = Math.max(max, nums[i]);
+
+        }
+        int[] arr = new int[max+1];
+
+
+        for(int i = 0; i <nums.length; i++){
+
+            int num = nums[i];
+            arr[num] += num;
+        }
+           int[] dp = new int[max+1];
+         dp[0] = arr[0];
+         dp[1] = Math.max(arr[0], arr[1]);
+
+         for(int i = 2; i <=max ; i++){
+            dp[i] = Math.max(dp[i-1], arr[i]+dp[i-2]);
+         }
+
+         return dp[max];
+
+        
+    }
+}
